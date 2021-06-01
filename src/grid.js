dc4bdc5d326683b45849bac3b9d49654d85aa2a5
Daniel <danielwu116@gmail.com>
2021-06-01 17:34:09 -0400

    Add Grid#connectNodes, Grid#attachNodeEvents, refactor mousedown event to add/remove target node from neighbors

--- src/grid.js
+++ src/grid.js
@@ -4,12 +4,84 @@ class Grid {
   constructor() {
     this.edit = true;
     this.drag = false;
-    this.graph = this.makeGrid();
     this.object = 'wall';
+    this.graph = this.makeGrid();
+
+    this.connectNodes();
   }
 
   toggleEdit() {
     this.edit = !this.edit;
+  }
+
+  connectNodes() {
+    const neighborDeltas = [
+      [-1, -1],
+      [-1,  0],
+      [-1,  1],
+      [ 0, -1],
+      [ 0,  1],
+      [ 1, -1],
+      [ 1,  0],
+      [ 1,  1]
+    ];
+
+    this.graph.forEach((row, i) => {
+      row.forEach((node, j) => {
+        neighborDeltas.forEach(delta => {
+          const posY = i + delta[0];
+          const posX = j + delta[1];
+          
+          if (this.graph[posY] && this.graph[posY][posX]) {
+            node.neighbors.push(this.graph[posY][posX]);
+          }
+        });
+      });
+    });
+  }
+
+  attachNodeEvents(node) {
+    node.addEventListener('mousedown', (e) => {
+      if (this.edit) {
+        switch (this.object) {
+          case 'sweepa':
+            document.getElementById('sweepa').id = "";
+            e.target.id = 'sweepa';
+            break;
+          case 'dust':
+            e.target.classList.toggle(this.object);
+            this.drag = true;
+          case 'wall':
+            const pos = e.target.id.split('-');
+            const graphNode = this.graph[pos[0]][pos[1]];
+
+            for (let nodeNeighbor of graphNode.neighbors) {
+              if (e.target.className.includes('wall')) {
+                nodeNeighbor.neighbors.push(graphNode);
+              } else {
+                let idx = nodeNeighbor.neighbors.indexOf(graphNode);
+                nodeNeighbor.neighbors.splice(idx, 1);
+              }
+            }
+            
+            e.target.classList.toggle(this.object);
+            this.drag = true;
+            break;
+          default:
+            break;
+        }
+      }
+    });
+
+    node.addEventListener('mouseover', (e) => {
+      if (this.drag) {
+        e.target.classList.toggle(this.object);
+      }
+    });
+
+    node.addEventListener('mouseup', () => {
+      this.drag = false;
+    });
   }
 
   makeGrid() {
@@ -21,37 +93,27 @@ class Grid {
     grid.addEventListener('mouseleave', () => this.drag = false);
 
     for (let i = 0; i < numRows; i++) {
-      const row = document.createElement('div');
-      row.className = `row`;
-      row.id = `${i}`;
+      const graphRow = [];
+      const gridRow = document.createElement('div');
+      gridRow.className = `row`;
+      gridRow.id = `${i}`;
 
       for (let j = 0; j < numCols; j++) {
-        const node = document.createElement('div');
-        node.className = `node unvisited`;
-        node.id = `${i}-${j}`;
-        graph.push(new GraphNode(node.id));
+        const newNode = document.createElement('div');
+        
+        newNode.className = `node unvisited`;
+        newNode.id = `${i}-${j}`;
 
-        node.addEventListener('mousedown', (e) => {
-          if (this.edit) {
-            e.target.classList.toggle('wall');
-            this.drag = true;
-          }
-        });
-
-        node.addEventListener('mouseover', (e) => {
-          if (this.drag) e.target.classList.toggle('wall');
-        });
-
-        node.addEventListener('mouseup', () => {
-          this.drag = false;
-        });
-
-        row.append(node);
+        this.attachNodeEvents(newNode);
+        gridRow.append(newNode);
+        
+        graphRow.push(new GraphNode(newNode.id));
       }
 
-      grid.append(row);
+      grid.append(gridRow);
+      graph.push(graphRow);
     }
-
+    
     return graph;
   }
 }
